004beee56d9bce3fe798441e6151138ec07c5dff
unknown <lee.eunseong7856@gmail.com>
2025-05-27 20:40:34 -0500

    hide un-finished links for now

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -25,11 +25,11 @@ const navItems = [
     icon: BookOpen,
     href: '/get-started',
   },
-  {
-    name: 'Saved Ideas',
-    icon: Bookmark,
-    href: '/saved',
-  },
+  // {
+  //   name: 'Saved Ideas',
+  //   icon: Bookmark,
+  //   href: '/saved',
+  // },
   {
     name: 'Idea Feed',
     icon: Sparkles,
@@ -113,7 +113,7 @@ export default function SideNav() {
         </nav>
         {/* Bottom nav */}
         <div className="p-4 border-t border-gray-200 dark:border-gray-800 space-y-1">
-          {bottomNavItems.map((item) => (
+          {/* {bottomNavItems.map((item) => (
             <a
               key={item.name}
               href={item.href}
@@ -122,7 +122,7 @@ export default function SideNav() {
               <item.icon className="w-5 h-5" />
               <span>{item.name}</span>
             </a>
-          ))}
+          ))} */}
           <button
             className="w-full flex items-center space-x-2 px-3 py-2 mt-2 rounded-lg text-red-600 hover:text-red-700 dark:text-red-500 dark:hover:text-red-400 hover:bg-red-50 dark:hover:bg-red-900/20 transition-colors"
             onClick={logout}
